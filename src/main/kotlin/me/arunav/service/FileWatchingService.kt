c5c13236cb7e5e67838ea945494e56b99526a2f5
Arunav Bhattacharya <itsarunav4u@gmail.com>
2022-04-27 00:24:06 -0700

    WatchService Sample

---
+++ src/main/kotlin/me/arunav/service/FileWatchingService.kt
@@ -0,0 +1,33 @@
+package me.arunav.service
+
+import java.nio.file.FileSystems
+import java.nio.file.Paths
+import java.nio.file.StandardWatchEventKinds.ENTRY_CREATE
+import java.nio.file.StandardWatchEventKinds.ENTRY_DELETE
+import java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY
+import java.nio.file.WatchService
+
+class FileWatchingService {
+
+    private val DIRECTORY_PATH = "/Users/arunav/Documents/workspace/scheduler-file-watcher"
+
+    fun poll() {
+        val watchService: WatchService = FileSystems.getDefault().newWatchService()
+        val path = Paths.get(DIRECTORY_PATH)
+        println(path.toAbsolutePath())
+        path.register(watchService, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY)
+        while (true) {
+            val watchKey = watchService.poll()
+            if (watchKey != null) {
+                for (event in watchKey.pollEvents()) {
+                    println("Event kind = ${event.kind()}, Event.context = ${event.context()}")
+                }
+                watchKey.reset()
+            }
+        }
+    }
+}
+
+fun main() {
+    FileWatchingService().poll()
+}
